33113f0f51505a5c27e19c650073c9901aa12448
Florian Schneider <florian.schneider@univ-montp2.fr>
2015-01-29 14:00:18 +0100

    include runCA and animateCA functions

--- code/simfunctions.r
+++ code/simfunctions.r
@@ -1,20 +1,217 @@
+########################################################
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Florian D. Schneider & Sonia Kéfi
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#   
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+########################################################
 
-count  <- function(x, neighbor, state = NULL) {
-			if(is.null(state)) state = neighbor
-			
-			neighbors <- numeric(length = prod(x$dim))
-			x_logical_with_border <- (x$cells %in% neighbor)[x_with_border]
-			for(k in interact) {
-				neighbors <- neighbors + x_logical_with_border[x_to_evaluate+k]
-			}
-			return(neighbors)	
+
+######################
+## mapping function ##
+######################
+
+# required to vectorise the counting and plotting. 
+# returns a map of the landscape to translate it into a vector with boundaries and another one to back-translate it to a vector without boundaries into the global environment. Needs to be called only once for the dimensions of the lattice. 
+
+
+mapping <- function(width, height, boundary = "periodic", i_matrix = matrix(c(0,1,0,1,NA,1,0,1,0), ncol = 3, byrow = TRUE)) {
+    
+  # derive helper vectors for counting: 
+  # transformation vector for evaluation at the border of the grid
+  # set evaluation matrix 
+  X <- matrix(as.integer(1:(width*height)), ncol = width, byrow =TRUE)
+  # setting the border of the evaluation matrix X
+  X <- cbind(X[,width], X, X[,1] )  
+  X <- rbind(X[height,], X, X[1,] ) 
+  # transformation vector which adds the border to the lattice:
+  x_with_border <- as.integer(t(X))
+  
+  assign("x_with_border", as.integer(t(X))  , envir = .GlobalEnv )
+  
+  # from the matrix X (with copied border cells), which cells are the actual cells (reverse transformation vector of the previous lines) 
+  #x_to_evaluate <- sort(matrix(1:prod(dim(X)), ncol = dim(X)[2], byrow =TRUE)[-c(1, dim(X)[1]), -c(1,dim(X)[2])]  )  	
+  
+  
+  assign("x_to_evaluate", sort(matrix(1:prod(dim(X)), ncol = dim(X)[2], byrow =TRUE)[-c(1, dim(X)[1]), -c(1,dim(X)[2])]  )	, envir = .GlobalEnv )
+  
+  
+  # defining the neighborhood which is to be evaluated	
+  # set interaction matrix
+  I <- i_matrix	
+  # coordinates of neighbours in Interaction matrix I: 
+  neighbours_in_I <- which(is.finite(abs(I)/abs(I)), arr.in = TRUE)
+  # coordinates relative to the evaluated cell (=  which(is.na(I) ) 
+  relrow <- neighbours_in_I[,1]-which(is.na(I), arr.ind = TRUE)[1]
+  relcol <- neighbours_in_I[,2]-which(is.na(I), arr.ind = TRUE)[2]
+  
+  # relative position of the four direct neighbours of a cell
+  #interact <- (relrow * dim(X)[2] + relcol)
+  
+  assign("interact", relrow * dim(X)[2] + relcol, envir = .GlobalEnv )
+  
 }
+
+
+
+####################
+## count function ##
+####################
+
+count  <- function(x, neighbor) {
+  
+  neighbors <- numeric(length = prod(x$dim))
+  x_logical_with_border <- (x$cells %in% neighbor)[x_with_border]
+  for(k in interact) {
+    neighbors <- neighbors + x_logical_with_border[x_to_evaluate+k]
+  }
+  return(neighbors)  
+}
+
+
+#################
+## patch count ##
+#################
+
+# identify and count the size of individual patches
+
+patches <- function(x, state) {
+  pattern <- x$cells
+  pattern <- pattern %in% state
+  map <- rep(NA, times = prod(x$dim))
+  old <- rep(99, times = prod(x$dim)) 
+  
+  while(!identical(old[pattern], map[pattern])) {
+    old <- map
+    count = as.integer(1)
+    for(i in which(pattern)) {
+      neighbors <- map[x_with_border][x_to_evaluate[i]+interact]
+      if(all(is.na(neighbors)) ) { 
+        map[i] <- count
+      } else {
+        map[i] <- min(neighbors, na.rm = TRUE)
+      }
+      count <- count +1
+    }
+    
+  }
+  
+  map <- as.factor(map)
+  patchvec <- as.vector(sapply(levels(map), function(i) length(which(map == i) ) )) 
+  
+  out <- vector()
+  if(length(patchvec) > 0) out <- sort(patchvec) else out <- NA
+  #out <- data.frame(size = unique(out), freq = sapply(unique(out), function(i) length(which(out >= i)) ))
+  return(out)
+  
+} 
+
+
+########################
+## fitting Power Laws ##
+########################
+
+
+fitPL <- function(psd, p_spanning, n = NULL) {
+  
+  # code of fitted classes
+  
+  n_plants <- sum(psd$size * psd$n)/n
+  
+  out <- list()
+  out$best <- NA
+  out$AIC <- vector("numeric", length = 3)
+  out$dAIC <- vector("numeric", length = 3)
+  
+  # criteria for vegetated state & desert state
+  
+  ##### linear power law model for parameter estimation
+  PLlm <- lm(I(log(p)) ~  1 - I(log(size)) , data = psd) 
+  
+  ###########
+  
+  try( {out$TPLdown <- nls(I(log(p)) ~ alpha * log(size) + Sx * (1 - size) , 
+                           data = psd,
+                           start = list(alpha =  PLlm$coefficients, Sx = 1/1000),
+                           #algorithm = "port",
+                           trace = FALSE
+  )}, silent = TRUE
+  )    
+  
+  if(!is.null(out$TPLdown) & !coefficients(out$TPLdown)["Sx"] <= 0) {
+    out$AIC[1] <- AIC(out$TPLdown) 
+  } else {
+    out$TPLdown <- list(NA)
+    out$AIC[1] <- NA
+  }
+  
+  #####
+  
+  try({out$PL <- nls(I(log(p)) ~ alpha * log(size), 
+                     data = psd,
+                     start = list( alpha =  PLlm$coefficients ),
+                     trace = FALSE,
+                     nls.control(maxiter = 50)
+  )}, silent = TRUE
+  )
+  
+  if(!is.null(out$PL)) {
+    out$AIC[2] <- AIC(out$PL)
+  } else {
+    out$PL  <- list(NA)
+    out$AIC[2] <- NA
+  }
+  
+  ###########
+  
+  try({out$TPLup <- nls(I(log(p)) ~  log(b) + log(1+(size^(alpha))/b ) , 
+                        data = psd,
+                        start = list( alpha =  PLlm$coefficients, b = p_spanning ) , 
+                        nls.control(maxiter = 50)
+  )}, silent = TRUE
+  )
+  
+  
+  if(!is.null(out$TPLup)) {
+    out$AIC[3] <- AIC(out$TPLup) 
+  } else { 
+    #result$fit$summary$TPLup  <- list(NA)
+    out$TPLup  <- list(NA)
+    out$AIC[3] <- NA
+  }
+  
+  ###########
+  
+  out$dAIC <-   out$AIC -min(out$AIC, na.rm = TRUE)
+  
+  out$best <- which.min(out$AIC)+1
+  
+  return(out)
+} 
+
 
 # plotting function for objects of class "landscape".
 plot.landscape <- function(x, grid = FALSE, axis = FALSE, cols = "auto", add = FALSE, ani = FALSE, ...) {
   lvls <- levels(x$cells) 
   nlev <- length(lvls)
-  if(cols[1] == "auto") cols = c("black","grey80", "white")  # default color value
+  if(cols[1] == "auto") cols = c("black", "white")  # default color value
   
   if(ani & Sys.info()[['sysname']] == "Windows") adj = -0.5 else adj = 0 #this adjustment constant is added when producing a pixel accurate png or gif file, requires TRUE when the function is used to plot animated figures. 
   
@@ -28,37 +225,130 @@ plot.landscape <- function(x, grid = FALSE, axis = FALSE, cols = "auto", add = F
   rect(rep(1:x$dim[1], times = x$dim[2])-.5, rep(1:x$dim[2], each = x$dim[1])-.5, rep(1:x$dim[1], times = x$dim[2])+.5, rep(1:x$dim[2], each = x$dim[1])+.5, col = cols[as.numeric(x$cells)], border = border)
   
   if(grid) box()
- }
+}
 
-# get patch size and patchsize distribution
-patches <- function(x, state, cumulative = TRUE) {
-	pattern <- x$cells
-	pattern <- pattern %in% state
-	map <- rep(NA, times = prod(x$dim))
-	old <- rep(99, times = prod(x$dim)) 
-	
-	while(!identical(old[pattern], map[pattern])) {
-		old <- map
-		count = as.integer(1)
-		for(i in which(pattern)) {
-			neighbors <- map[x_with_border][x_to_evaluate[i]+interact]
-			if(all(is.na(neighbors)) ) { 
-				map[i] <- count
-			} else {
-				map[i] <- min(neighbors, na.rm = TRUE)
-			}
-				count <- count +1
-			}
 
-		}
-	
-	map <- as.factor(map)
-	patchvec <- as.vector(sapply(levels(map), function(i) length(which(map == i) ) )) 
-	
-	out <- vector()
-	if(length(patchvec) > 0) out <- sort(patchvec) else out <- NA
-	#out <- data.frame(size = unique(out), freq = sapply(unique(out), function(i) length(which(out >= i)) ))
-	return(out)
-	
-	} 
+
+animateCA <- function(result, filename) {
+  # FIGURE 3 -- animated gif
+  library(animation)
+  if(Sys.info()[['sysname']] == "Linux") X11.options(antialias = "none") #for Linux Systems to enable pixel-wise plotting in (animated) gif-files. 
+  if(Sys.info()[['sysname']] == "Windows") windows.options(antialias = "none") #for Windows Systems to enable pixel-wise plotting in (animated) gif-files. 
+  
+  saveGIF( 
+    for(i in 1:length(result$timeseries) ) {
+      par(mar = c(0,0,0,0))
+      plot(result$timeseries[[i]], grid = FALSE, ani = TRUE) 
+    }
+    , movie.name = filename, img.name = "grid", convert = "convert", interval = 0.01/1,
+    cmd.fun = system, clean = TRUE, ani.width = width, ani.height = height, outdir = getwd())
+}
+
+
+runCA <- function(init, parms, width = 100, height = 100, delta = 0.1, t_max = 1000, t_min = 200, t_eval = 100, isstable = 0.000001, saveeach = 50) {
+  
+  # cell states:
+  states = c("1","0")  # vegetated, empty and degraded, respectively
+  
+  # mapping vectors: 
+  mapping(width, height)   # create mapping vectors (see documentation)
+  
+  # initialize landscape: 
+  parms_temp <- parms   # set temporary parameter object
+  parms_temp$rho_one <- init   # draw initial vegetation cover
+  
+  init_plant <- as.integer(width*height*parms_temp$rho_one) # how many initial plants
+  cells <- factor(rep("0", times = width*height), levels = states) # vector of empty cells in state "0"
+  cells[sample(1:(width*height), init_plant, replace = FALSE)] <- "1"  # replace init_plant cells, randomly drawn, with "1"
+  
+  # wrap landscape object:
+  initial <- list(  
+    dim = c(as.integer(width), as.integer(height)),  # first element contains the dimensions of the landscape 
+    cells = cells  #contains a random row-wise, factorial vector to fill the grid 
+  )
+  levels(initial$cells) <- states  #assign cell states 
+  class(initial) <- c("list","landscape") # set class of object (required for plotting)
+  
+  # calculate timesteps for snapshots:
+  n_snaps <- length(seq(0,2*t_eval,saveeach))
+  t_snaps <-  ceiling(t_max/(saveeach*n_snaps))*(n_snaps*saveeach)
+  snapshots <- data.frame(time = seq(0, t_snaps, saveeach), i= seq(0, t_snaps, saveeach)/delta+1, pos = c(1:length(seq(0, t_snaps, saveeach))) )
+  
+  # initialise result object:
+  result <- list()  # generate list object
+  result$time <- seq(0, t_min, delta) # add vector of realized timesteps
+  result$rho_one <- vector("numeric", length = length(result$time))  # allocate vector for global vegetation cover 
+  result$rho_one[1] <- parms_temp$rho_one   # fill in first value of vegetation cover
+  result$q_one_one  <- vector("numeric", length = length(result$time))  # allocate vector for average local cover
+  result$q_one_one[1]  <- mean(subset(count(initial, "1")/4, initial$cells == "1") )  # fill in first vector of average local cover
+  
+  result$timeseries <- list()
+  result$timeseries[[1]] <- initial
+  
+  # --------------- simulation -----------------
+  
+  # initialise simulation variables: 
+  x_old <- initial  # ghost matrix at t_i
+  stability <- 1  # check value for stability
+  i = 0  # iterator for simulation timesteps
+  
+  # starting iterations:
+  while(stability > isstable & i <= t_max/delta & parms_temp$rho_one > 0) {
+    
+    i <- i +1  # increase iterator
+    x_new <- x_old   	# copy x_old into an object x_new to allocate memory	
+    
+    # model specific part:
+    # 1 - setting time-step parameters
+    #parms_temp$local <- count(x_old, "1")/4  # count local density of occupied fields for each cell:
+    
+    # 2 - drawing random numbers
+    rnum <- runif(width*height) # one random number between 0 and 1 for each cell
+    
+    # 3 - setting transition probabilities
+    growth <- with(parms_temp, r * b * rho_one * ( 1 - (rho_one / K)) / (1 - rho_one)  *delta)  # recolonisation rates of all cells 
+    
+    death <- with(parms_temp, m + ( a * L )/( 1 + a * h * rho_one ) *delta)   # set probability of death for each cell
+    
+    # check for sum of probabilities to be inferior 1 and superior 0
+    if(any(c(growth, death) > 1 )) warning(paste("a set probability is exceeding 1 in time step", i, "! decrease delta!!!")) 
+    if(any(c(growth, death) < 0)) warning(paste("a set probability falls below 0 in time step", i, "! balance parameters!!!")) 
+    
+    # 4 - apply transition probabilities  
+    
+    x_new$cells[which(x_old$cells == "0" & rnum <= growth)] <- "1"
+    x_new$cells[which(x_old$cells == "1" & rnum <= death)] <- "0"
+    
+    # 5 - saving state of the new grid		
+    
+    parms_temp$rho_one <- sum(x_new$cells == "1")/(width*height) # store current global vegetation cover
+    result$rho_one[i] <- parms_temp$rho_one # write current vegetation cover to result
+    #result$q_one_one[i]  <- mean(subset(count(x_new, "1")/4, x_new$cells == "1") ) # write current average local cover to result
+    x_old <- x_new # replace ghost matrix for next iteration
+    
+    
+    if(i %in% snapshots$i) {  
+      result$timeseries[[match(i, snapshots$i)]] <- x_new
+    }
+    
+    
+    if(i > t_min/delta+1) { # if we are over the minimal timespan 
+      t_1 <- (i-2*t_eval/delta):(i-t_eval/delta)-1 # vector of t_eval timesteps previous to the last t_eval timesteps
+      t_2 <- (i-t_eval/delta):(i) # vector of the last t_eval timesteps 
+      
+      if(parms_temp$rho_one > 0) { 
+        stability <- (abs(mean(result$rho_one[t_1]) - mean(result$rho_one[t_2])))/(mean(result$rho_one[t_1])) # calculate stability, i.e. difference between the mean cover in the two evaluation periods t_1 and t_2 
+      } else {
+        stability <- 0 # set stability to 0 if cover is 0, immediate stop of simulation
+      }
+      
+      result$time[i] <- i*delta # save timestep to results
+      
+    }
+    
+  } 
+  
+  return(result)
+}
+
 
